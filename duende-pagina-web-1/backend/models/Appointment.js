c015f41b4905c89af387ce594e7e6b3b52f57347
Nahomi Bolaños <120617689+nahomibv@users.noreply.github.com>
2023-11-19 12:19:15 -0600

    Change Appointment Schema

--- duende-pagina-web-1/backend/models/Appointment.js
+++ duende-pagina-web-1/backend/models/Appointment.js
@@ -2,15 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const appointmentSchema = new Schema({
-    participants: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-    type: String,
-    details: String,
-    date: Date,
-    image: String,
+    Subject: String,
+    EventType: String,
+    StartTime: Date,
+    EndTime: Date,
+    Details: String,
     status: String,
-    startingTime: Date,
-    endingTime: Date,
-    orderNumber: String
 });
 
 module.exports = mongoose.model('Appointment', appointmentSchema);
