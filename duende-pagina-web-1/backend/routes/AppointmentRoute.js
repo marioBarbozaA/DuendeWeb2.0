bd12635f9afd14ed5f06f9e30248a15b21a25b3b
Nahomi Bolaños <120617689+nahomibv@users.noreply.github.com>
2023-11-22 21:33:59 -0600

    Update AppointmentRoute.js

--- duende-pagina-web-1/backend/routes/AppointmentRoute.js
+++ duende-pagina-web-1/backend/routes/AppointmentRoute.js
@@ -2,5 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Appointment = require('./../controllers/AppointmentController.js');
 
+router.route('/create').post(authRequire, appointmentControllers.createAppointment);
+router.route('/update').put(authRequire, appointmentControllers.updateAppointment);
+router.route('/delete').delete(authRequire, appointmentControllers.deleteAppointment);
+router.route('/getAll').get(authRequire, appointmentControllers.getAllAppointments);
 
 module.exports = router;
