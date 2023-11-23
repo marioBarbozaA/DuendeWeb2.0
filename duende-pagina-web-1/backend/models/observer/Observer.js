2ae234ed4b87afb7d247f390834975954178b57b
Camilo Sanchez <camsanchez@estudiantec.cr>
2023-11-22 19:32:50 -0600

    Update sales controller and observer

--- duende-pagina-web-1/backend/models/observer/Observer.js
+++ duende-pagina-web-1/backend/models/observer/Observer.js
@@ -22,7 +22,7 @@ class Observer {
 
 class newSaleObserver extends Observer {
     async update(message) {
-        
+
         //Admin Message
         const notificationDataAdmin = {
             user: "6539c5111b8018f186929836",
@@ -43,7 +43,7 @@ class newSaleObserver extends Observer {
         };
 
         console.log('Received notification data (after):', notificationData);
-    
+
         try {
             const notificationC = await SingletonDAO.createNotification(notificationDataClient);
             const notificationA = await SingletonDAO.createNotification(notificationDataAdmin);
@@ -56,10 +56,35 @@ class newSaleObserver extends Observer {
 }
 
 class updateSaleObserver extends Observer {
-    update(message) {
-        // Lógica específica para el observador de ventas
+    async update(message) {
         console.log(`SaleObserver update: ${message}`);
-        // Aquí puedes poner la lógica que necesites cuando se actualiza la venta
+        //Admin Message
+        const notificationDataAdmin = {
+            user: "6539c5111b8018f186929836",
+            title: "New Sale",
+            description: "A new sale has been made",
+            date: new Date(),
+            type: "New Order",
+            state: "Active",
+        };
+        //Cliente Message
+        const notificationDataClient = {
+            user: message.userBuyer,
+            title: "Purchase Completed",
+            description: "Your sale was made successfully",
+            date: new Date(),
+            type: "Confirm Purchase",
+            state: "Active",
+        };
+
+        try {
+            const notificationC = await SingletonDAO.createNotification(notificationDataClient);
+            const notificationA = await SingletonDAO.createNotification(notificationDataAdmin);
+            res.status(201).json(notificationC);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ msg: 'Server error' + error });
+        }
     }
 }
 
