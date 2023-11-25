2c33b40951e179a58663fc8001c3c96875836dbd
Camilo Sanchez <camsanchez@estudiantec.cr>
2023-11-25 01:45:29 -0600

    add Agenda
    Added the agenda code

--- duende-pagina-web-1/frontend/src/pages/Agenda/Agenda.js
+++ duende-pagina-web-1/frontend/src/pages/Agenda/Agenda.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import NavBar from '../../Components/NavBar/NavBar';
 import Logo from '../../Imagenes/Logo-Duende.png';
 import './Agenda.css';
@@ -10,110 +10,464 @@ import {
 	WorkWeek,
 	Month,
 	Agenda,
-	EventSettingsModel,
-	PopupOpenEventArgs,
-	FieldValidationEventArgs,
+	ViewsDirective,
+	ViewDirective,
+	DragAndDrop,
+	Resize,
 } from '@syncfusion/ej2-react-schedule';
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
+import { DateTimePickerComponent } from '@syncfusion/ej2-react-calendars';
 
 import { registerLicense } from '@syncfusion/ej2-base';
-
-import { DataManager,WebApiAdaptor } from '@syncfusion/ej2-data' ;
+import { L10n } from '@syncfusion/ej2-base';
+import axios from 'axios';
 
 registerLicense(
 	'Ngo9BigBOggjHTQxAR8/V1NHaF5cWWdCf1FpRGRGfV5yd0VHYlZQRHxeSk0SNHVRdkdgWH5fd3RVR2FYVkx2Vko=',
 );
 
-//We have received your application and created a ticket with the reference number #523297. We will validate your request within the next 48
-export default class Scheduler extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			selectedEventType: 'Tipo1', // Valor predeterminado
-		};
-	}
-	 localData: EventSettingsModel = {
-		dataSource: [
-			{
-				End: new Date(2019, 0, 11, 6, 30),
-				Start: new Date(2019, 0, 11, 4, 0),
-				Summary: '',
-				IsAllDay: true,
-				RecurrenceRule: 'FREQ=DAILY;INTERVAL=1;COUNT=5',
-				isReadonly: true,
-				Type:'Tipo1'
-			},
-			{
-				Id:2,
-				End: new Date(2019, 0, 21, 8,30),
-				Start: new Date(2019, 0, 21, 7, 0),
-				Summary:'Entrega',
-				isReadonly: true,
-				Type:'Tipo2'
+L10n.load({
+	'en-US': {
+		schedule: {
+			saveButton: 'Guardar',
+			cancelButton: 'Cerrar',
+			deleteButton: 'Eliminar',
+			newEvent: 'Añadir Evento',
+		},
+	},
+});
 
+const Scheduler = () => {
+	const [eventTypes, setEventTypes] = useState({});
+
+	const [localData, setLocalData] = useState([]);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get(
+					'http://localhost:3500/appointments/getAll',
+				);
+				console.log('Datos recibidos de la API:', response.data);
+				if (Array.isArray(response.data)) {
+					// Asegúrate de que la estructura de los datos coincida con lo que espera el componente
+					const formattedData = response.data.map(item => {
+						// Transforma los datos aquí si es necesario
+						return item;
+					});
+					setLocalData(formattedData);
+				} else {
+					console.error(
+						'La respuesta de la API no es un arreglo:',
+						response.data,
+					);
+				}
+			} catch (error) {
+				console.error('Error al obtener datos de la API:', error);
 			}
-		],
-		fields: {
-			subject: { name: 'Summary', default: 'No title' },
-			startTime: { name:'Start'},
-			endTime: { name:'End'},
-			id: 'Id',
-			type: 'Type', // Agregar el tipo de evento
-		}
-	};
-	
-	handleEventTypeChange = (event) => {
-		this.setState({
-			selectedEventType: event.target.value,
+		};
+
+		fetchData();
+	}, []);
+	useEffect(() => {
+		const initialEventTypes = {};
+		localData.forEach(event => {
+			initialEventTypes[event.Id] = event.EventType || 'Otro';
 		});
+		setEventTypes(initialEventTypes);
+	}, [localData]);
+
+	const fieldsData = {
+		id: 'Id',
+		subject: { name: 'Subject', validation: { required: true } },
+		type: { name: 'EventType', validation: { required: true } },
+		details: { name: 'Details', validation: { required: true } },
+		startTime: { name: 'StartTime', validation: { required: true } },
+		endTime: { name: 'EndTime', validation: { required: true } },
+		CustomerName: { name: 'CustomerName' },
+		ReferenceService: { name: 'ReferenceService' },
+		OrderNumber: { name: 'OrderNumber' },
+		DeliveryCustomerName: { name: 'DeliveryCustomerName' },
 	};
-	handlePopupOpen = (args: PopupOpenEventArgs) => { 
-        if (args.type === 'Editor' && !this.state.isEventTypeFieldAdded) { 
-            const formElements = args.element.querySelector('.e-schedule-form'); 
-            if (formElements) { 
-				console.log("Form elements ", formElements);
-                const eventTypeElement = document.createElement('select'); 
-                eventTypeElement.id = 'EventType'; 
-                eventTypeElement.name = 'EventType'; 
- 
-                const option1 = document.createElement('option'); 
-                option1.value = 'Tipo1'; 
-                option1.text = 'Tipo1'; 
-                eventTypeElement.appendChild(option1); 
- 
-                const option2 = document.createElement('option'); 
-                option2.value = 'Tipo2'; 
-                option2.text = 'Tipo2'; 
-                eventTypeElement.appendChild(option2); 
- 
-                eventTypeElement.value = this.state.selectedEventType; 
- 
-                formElements.appendChild(eventTypeElement); 
- 
-                this.setState((prevState) => ({ 
-                    isEventTypeFieldAdded: !prevState.isEventTypeFieldAdded, 
-                })); 
-            } 
-        } 
-    }; 
-	handleFieldValidation = (args: FieldValidationEventArgs) => {
-		if (args.field === 'Subject' && args.value === '') {
-			args.errorClass.push('e-schedule-error');
-			args.errorMessage.push('Subject is required');
+	const onDragStart = args => {
+		//args.scroll = { enabled: false };
+		args.interval = 10;
+	};
+
+	const onResizeStart = args => {
+		//args.scroll = { enabled: false };
+		args.interval = 10;
+	};
+
+	const handleActionBegin = async args => {
+		if (
+			!['eventCreate', 'eventChange', 'eventRemove'].includes(args.requestType)
+		) {
+			return;
+		}
+		const eventData =
+			args.data && args.data instanceof Array ? args.data[0] : args.data;
+
+		if (!eventData) {
+			console.error('No se encontraron datos de evento.');
+			return;
+		}
+		console.log('EventData:', eventData);
+
+		const isUpdate = eventData._id != null;
+		const apiEndpoint = isUpdate ? '/update' : '/create';
+
+		if (
+			args.requestType === 'eventCreate' ||
+			args.requestType === 'eventChange'
+		) {
+			args.cancel = true;
+
+			try {
+				const response = await axios({
+					method: isUpdate ? 'put' : 'post',
+					url: `http://localhost:3500/appointments${apiEndpoint}`,
+					data: eventData,
+				});
+
+				console.log('API response for save', response); // Para depuración
+
+				setLocalData(prevData => {
+					if (isUpdate) {
+						return prevData.map(event =>
+							event.Id === eventData.Id
+								? { ...event, ...response.data }
+								: event,
+						);
+					} else {
+						return [...prevData, response.data];
+					}
+				});
+			} catch (error) {
+				console.error('Error al guardar los datos del evento:', error);
+				console.log('Error data', error.response || error.message);
+			}
+		} else if (args.requestType === 'eventRemove') {
+			args.cancel = true;
+			const eventId = eventData._id;
+			console.log('event id ', eventId);
+
+			if (!eventId) {
+				console.error('No se pudo obtener el ID del evento para eliminar.');
+				return;
+			}
+
+			try {
+				const response = await axios.delete(
+					'http://localhost:3500/appointments/delete',
+					{
+						data: { id: eventId },
+					},
+				);
+
+				console.log('API response for delete', response); // Para depuración
+
+				setLocalData(prevData =>
+					prevData.filter(event => event._id !== eventId),
+				);
+			} catch (error) {
+				console.error('Error al eliminar el evento:', error);
+				console.log('Error data', error.response || error.message); // Para depuración
+			}
 		}
 	};
-	handleEventSave = (args) => {
-		// Agregar o actualizar el campo Type al evento antes de guardarlo
-		args.data.Type = this.state.selectedEventType;
-	
-		// Lógica de guardado del evento, puedes ajustar según tus necesidades
-		console.log('Evento guardado:', args.data);
-	
-		// Actualizar el estado para forzar una re-renderización del componente
-		this.forceUpdate();
-	};
-	render() {
+
+	const Cita = props => {
+		//console.log('Cita props', props);
+		//console.log(Object.keys(props).length);
+
 		return (
-			<>
+			<table className='custom-event-editor'>
+				<tbody>
+					<tr>
+						<td className='e-textlabel'>Evento</td>
+						<td>
+							<input
+								id='Summary'
+								name='Subject'
+								type='text'
+								className='e-field e-input'
+							/>
+						</td>
+					</tr>
+					<tr>
+						<td className='e-textlabel'>Tipo</td>
+						<td>
+							<DropDownListComponent
+								id='EventType'
+								dataSource={['Entrega', 'Cita', 'Otro']}
+								placeholder='Escoge el tipo'
+								data-name='EventType'
+								value={eventTypes[props.Id]}
+								className='e-field'
+								enabled={
+									Object.keys(props).length <= 0 ||
+									Object.keys(props).length == 3
+								}
+								change={e => {
+									setEventTypes(prevEventTypes => ({
+										...prevEventTypes,
+										[props.Id]: e.value,
+									}));
+								}}
+							></DropDownListComponent>
+						</td>
+					</tr>
+
+					<tr>
+						<td className='e-textlabel'>Nombre del Cliente</td>
+						<td>
+							<input
+								id='CustomerName'
+								name='CustomerName'
+								type='text'
+								className='e-field e-input'
+							/>
+						</td>
+					</tr>
+
+					<tr>
+						<td className='e-textlabel'>Imagen de Referencia</td>
+						<td>
+							<input
+								id='ReferenceService'
+								name='ReferenceService'
+								type='text' // Puedes cambiar esto según tus necesidades
+								className='e-field e-input'
+							/>
+						</td>
+					</tr>
+
+					<tr>
+						<td className='e-textlabel'>De</td>
+						<td>
+							<DateTimePickerComponent
+								id='StartTime'
+								data-name='StartTime'
+								value={new Date(props.startTime || props.StartTime)}
+								format='dd/MM/yy hh:mm a'
+								className='e-field'
+							></DateTimePickerComponent>
+						</td>
+					</tr>
+					<tr>
+						<td className='e-textlabel'>Hasta</td>
+						<td>
+							<DateTimePickerComponent
+								id='EndTime'
+								data-name='EndTime'
+								value={new Date(props.endTime || props.EndTime)}
+								format='dd/MM/yy hh:mm a'
+								className='e-field'
+							></DateTimePickerComponent>
+						</td>
+					</tr>
+					<tr>
+						<td className='e-textlabel'>Detalles</td>
+						<td>
+							<input
+								id='Details'
+								name='Details'
+								type='text'
+								className='e-field e-input'
+							/>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		);
+	};
+	const Entrega = props => {
+		//console.log('Entrega props', props);
+		//console.log(Object.keys(props).length);
+
+		return (
+			<table className='custom-event-editor'>
+				<tbody>
+					<tr>
+						<td className='e-textlabel'>Evento</td>
+						<td>
+							<input
+								id='Summary'
+								name='Subject'
+								type='text'
+								className='e-field e-input'
+							/>
+						</td>
+					</tr>
+					<tr>
+						<td className='e-textlabel'>Tipo</td>
+						<td>
+							<DropDownListComponent
+								id='EventType'
+								dataSource={['Entrega', 'Cita', 'Otro']}
+								placeholder='Escoge el tipo'
+								data-name='EventType'
+								value={eventTypes[props.Id]}
+								className='e-field'
+								enabled={
+									Object.keys(props).length <= 0 ||
+									Object.keys(props).length == 3
+								}
+								change={e => {
+									setEventTypes(prevEventTypes => ({
+										...prevEventTypes,
+										[props.Id]: e.value,
+									}));
+								}}
+							></DropDownListComponent>
+						</td>
+					</tr>
+
+					<tr>
+						<td className='e-textlabel'>Número de Pedido</td>
+						<td>
+							<input
+								id='OrderNumber'
+								name='OrderNumber'
+								type='text'
+								className='e-field e-input'
+							/>
+						</td>
+					</tr>
+
+					<tr>
+						<td className='e-textlabel'>Nombre del Cliente</td>
+						<td>
+							<input
+								id='DeliveryCustomerName'
+								name='DeliveryCustomerName'
+								type='text'
+								className='e-field e-input'
+							/>
+						</td>
+					</tr>
+
+					<tr>
+						<td className='e-textlabel'>De</td>
+						<td>
+							<DateTimePickerComponent
+								id='StartTime'
+								data-name='StartTime'
+								value={new Date(props.startTime || props.StartTime)}
+								format='dd/MM/yy hh:mm a'
+								className='e-field'
+							></DateTimePickerComponent>
+						</td>
+					</tr>
+					<tr>
+						<td className='e-textlabel'>Hasta</td>
+						<td>
+							<DateTimePickerComponent
+								id='EndTime'
+								data-name='EndTime'
+								value={new Date(props.endTime || props.EndTime)}
+								format='dd/MM/yy hh:mm a'
+								className='e-field'
+							></DateTimePickerComponent>
+						</td>
+					</tr>
+					<tr>
+						<td className='e-textlabel'>Detalles</td>
+						<td>
+							<input
+								id='Details'
+								name='Details'
+								type='text'
+								className='e-field e-input'
+							/>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		);
+	};
+	const Otra = props => {
+		//console.log('Otra props', props);
+		//console.log(Object.keys(props).length);
+		return (
+			<table className='custom-event-editor'>
+				<tbody>
+					<tr>
+						<td className='e-textlabel'>Evento</td>
+						<td>
+							<input
+								id='Summary'
+								name='Subject'
+								type='text'
+								className='e-field e-input'
+							/>
+						</td>
+					</tr>
+					<tr>
+						<td className='e-textlabel'>Tipo</td>
+						<td>
+							<DropDownListComponent
+								id='EventType'
+								dataSource={['Entrega', 'Cita', 'Otro']}
+								placeholder='Escoge el tipo'
+								data-name='EventType'
+								value={eventTypes[props.Id]}
+								className='e-field'
+								enabled={
+									Object.keys(props).length <= 0 ||
+									Object.keys(props).length == 3
+								}
+								change={e => {
+									setEventTypes(prevEventTypes => ({
+										...prevEventTypes,
+										[props.Id]: e.value,
+									}));
+								}}
+							></DropDownListComponent>
+						</td>
+					</tr>
+
+					<tr>
+						<td className='e-textlabel'>De</td>
+						<td>
+							<DateTimePickerComponent
+								id='StartTime'
+								data-name='StartTime'
+								value={new Date(props.startTime || props.StartTime)}
+								format='dd/MM/yy hh:mm a'
+								className='e-field'
+							></DateTimePickerComponent>
+						</td>
+					</tr>
+					<tr>
+						<td className='e-textlabel'>Hasta</td>
+						<td>
+							<DateTimePickerComponent
+								id='EndTime'
+								data-name='EndTime'
+								value={new Date(props.endTime || props.EndTime)}
+								format='dd/MM/yy hh:mm a'
+								className='e-field'
+							></DateTimePickerComponent>
+						</td>
+					</tr>
+					<tr>
+						<td className='e-textlabel'>Detalles</td>
+						<td>
+							<input
+								id='Details'
+								name='Details'
+								type='text'
+								className='e-field e-input'
+							/>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		);
+	};
+	return (
+		<>
 			<NavBar
 				imagen={Logo}
 				pathMain='MainPageAdmin'
@@ -123,24 +477,64 @@ export default class Scheduler extends Component {
 				pathTienda='MainPageEcomerceAdmin'
 				mostrarCarrito={false}
 			/>
-		
+
 			<ScheduleComponent
 				currentView='Month'
-				selectedDate={new Date(2019, 0, 11)}
-				eventSettings={ this.localData}
-				eventRendered={(args) => {
-					//console.log(args.data);
-					if (args.data.Type === 'Tipo2') {
-						args.element.style.backgroundColor = 'red';
-						args.element.style.color = 'white';
-					}
+				eventSettings={{
+					dataSource: localData,
+					fields: fieldsData,
 				}}
-				popupOpen={this.handlePopupOpen}
-				fieldValidation={this.handleFieldValidation}
-				eventSave={this.handleEventSave}>
-				<Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+				dragStart={onDragStart}
+				resizeStart={onResizeStart}
+				showQuickInfo={false}
+				editorTemplate={props =>
+					eventTypes[props.Id] === 'Cita'
+						? Cita(props)
+						: eventTypes[props.Id] === 'Entrega'
+							? Entrega(props)
+							: Otra(props)
+				}
+				eventRendered={props => {
+					const eventType = props.data.EventType;
+
+					let color = '';
+					switch (eventType) {
+						case 'Cita':
+							color = 'rgba(25, 130, 196, 0.3)'; // Azul con menos opacidad
+							break;
+						case 'Entrega':
+							color = 'rgba(255, 206, 86, 0.3)'; // Amarillo con menos opacidad
+							break;
+						default:
+							color = 'rgba(106, 76, 147, 0.3)'; // Púrpura con menos opacidad
+					}
+
+					props.element.style.backgroundColor = color;
+					props.element.style.border = `1px solid ${color.replace('0.3', '1')}`; // Borde con opacidad completa
+					props.element.style.color = 'black';
+				}}
+				actionBegin={handleActionBegin} // Añade el manejador de acción aquí
+			>
+				<ViewsDirective>
+					<ViewDirective
+						option='Day'
+						startHour='01:00'
+						interval={3}
+					></ViewDirective>
+					<ViewDirective option='Week'></ViewDirective>
+					<ViewDirective
+						option='Month'
+						showWeekNumber={true}
+						isSelected={true}
+					></ViewDirective>
+					<ViewDirective option='Agenda'></ViewDirective>
+				</ViewsDirective>
+
+				<Inject
+					services={[Day, Week, WorkWeek, Month, Agenda, DragAndDrop, Resize]}
+				/>
 			</ScheduleComponent>
-			</>
-		);
-	}
-}
+		</>
+	);
+};
+export default Scheduler;
